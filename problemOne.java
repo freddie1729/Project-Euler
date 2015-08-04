b07cd95ed01484e2a93d3d6a4f4281f8a72c6767
Freddie Wright <fpjwright@gmail.com>
2015-08-04 20:16:09 +0100

    Neatened code

--- problemOne.java
+++ problemOne.java
@@ -2,14 +2,14 @@ import java.lang.Math;
 public class problemOne {
 
     public static void main(String[] args) {
-      System.out.println(answer()); 
+      System.out.println(answer());
     }
-  
+
     public static int answer(){
       int sum = 0;
       for(int i=1; i<1000; i++){
         if((i%3==0)||(i%5==0)){
-          sum = sum + i;
+          sum += i;
         }
       }
       return sum;
