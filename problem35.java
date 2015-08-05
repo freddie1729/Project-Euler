07458f9ad68acab776be85ee5e489874e7ffd6a4
Freddie Wright <fpjwright@gmail.com>
2015-08-05 19:49:23 +0100

    Added solve method. TODO: Fix issue with the cycleDigits method to stop leading zeroes being lost.

--- problem35.java
+++ problem35.java
@@ -3,10 +3,42 @@ import java.lang.Math;
 public class problem35 {
 
     public static void main(String[] args) {
-      System.out.println(cycleDigits(Long.parseLong(args[0])));
+      System.out.println(solve(Long.parseLong(args[0])));
     }
 
     public static long cycleDigits(long n){
     	return Long.parseLong(String.valueOf(n).substring(1) + String.valueOf(n).substring(0,1));
     }
+
+    public static long solve(long max){
+    	long count = 0;
+    	int length = 1;
+    	int p = 10;
+    	long k;
+    	for(long i=2; i<max; i++){
+    		k = i;
+    		if(i%p == 0){
+    			length++;
+    			p *= 10;
+    		}
+    		Boolean allPrime = true;
+    		for(long j=0; j<length; j++){
+    			if(primes.isPrime(i)){
+    				System.out.println(i + "is prime! The next cycle is: " + cycleDigits(i));
+    				i = cycleDigits(i);
+    			} else {
+    				allPrime = false;
+    				break;
+    			}
+    		}
+    		if(allPrime){
+    			count++;
+    			System.out.println("Success! " + k + " " + count);
+    		}
+    		i = k;
+    	}
+    	return count;
+    }
+
+
 }
