5d62ca2820d47a39f01278443539c9fcea0f28af
Freddie Wright <fpjwright@gmail.com>
2015-08-05 19:17:32 +0100

    Added method for cycling digits of a number

--- problem35.java
+++ problem35.java
@@ -3,7 +3,10 @@ import java.lang.Math;
 public class problem35 {
 
     public static void main(String[] args) {
-      System.out.println();
+      System.out.println(cycleDigits(Long.parseLong(args[0])));
     }
-    
+
+    public static long cycleDigits(long n){
+    	return Long.parseLong(String.valueOf(n).substring(1) + String.valueOf(n).substring(0,1));
+    }
 }
