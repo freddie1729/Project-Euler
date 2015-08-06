c031fdc71f2ece8c5f7d08f9248a7a959cd1fdda
Freddie Wright <fpjwright@gmail.com>
2015-08-06 21:59:05 +0100

    New method found. Much quicker and it gives the correct answer.

--- problem40.java
+++ problem40.java
@@ -7,15 +7,30 @@ public class problem40 {
     }
 
     public static long solve(){
-        String number = "";
-        for(long i=0; number.length() <= 1000000; i++){
-            number += String.valueOf(i);
-        }
+        long digitLength = 1;
+        long position = 1;
+        long k = 10;
         long answer = 1;
-        for(long j=0; j<=6; j++){
-            answer *= Long.parseLong(number.substring((int)Math.pow(10, j)-1,(int)Math.pow(10, j)));
+        for(long i=1; true; i++){
+            if(i%k == 0){
+                k *= 10;
+                digitLength++;
+            }
+            if(Math.floor(Math.log10(position-1)) < Math.floor(Math.log10(position + digitLength - 1))){
+                for(int j = 0; j < digitLength; j++){
+                    if(isInteger(Math.log10(position + j))){
+                        answer *= Long.parseLong(String.valueOf(i).substring(j, j+1));
+                    }
+                }
+                if(position >= 1000000){
+                    return answer;
+                }
+            }
+            position += digitLength;
         }
-        return answer;
     }
 
+    public static Boolean isInteger(double n){
+        return Math.floor(n) == n;
+    }
 }
