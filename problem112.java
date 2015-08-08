fee1252cef9589089ef57fe9c6ac75744093d6b9
Freddie Wright <fpjwright@gmail.com>
2015-08-08 22:03:37 +0100

    Added method for determining whether a number is bouncy.

--- problem112.java
+++ problem112.java
@@ -3,6 +3,28 @@ import java.lang.Math;
 public class problem112 {
 
     public static void main(String[] args) {
-        System.out.println(); 
+        System.out.println(bouncy(Long.parseLong(args[0]))); 
+    }
+
+    public static Boolean bouncy(long n){
+        long m = n;
+        Boolean increasing = true;
+        Boolean decreasing = true;
+        long length = (long)Math.floor(Math.log10(n));
+        for(int i = 1; i<=length; i++){
+            if(m%10 < ((m-m%10)/10)%10){
+                increasing = false;
+                break;
+            }
+            m = (long)Math.floor(m/10);
+        }
+        for(int j = 1; j<=length; j++){
+            if(n%10 > ((n-n%10)/10)%10){
+                decreasing = false;
+                break;
+            }
+            n = (long)Math.floor(n/10);
+        }
+        return !increasing && !decreasing;
     }
 }
