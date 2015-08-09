55394d517be5359fd83b9c35cdb4a5bd6e37cc99
Freddie Wright <fpjwright@gmail.com>
2015-08-09 01:07:08 +0100

    Solving using a method to check every R until one low enough is found. Unfortunately, this takes too long.

--- problem243.java
+++ problem243.java
@@ -3,11 +3,22 @@ import java.lang.Math;
 public class problem243 {
 
     public static void main(String[] args) {
-        System.out.println(R(Long.parseLong(args[0])));
+        System.out.println(solve());
     }
 
     public static double R(long d){
         return (double)utils.phi(d)/((double)d-1);
+    }
+
+    public static long solve(){
+        for(long d = 1; true; d++){
+            if(R(d)<(double)15499/94744){
+                return d;
+            }
+            if(d%10000 == 0){
+                System.out.println(d);
+            }
+        }
     }
 
 
