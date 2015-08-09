402e271e8a4f6f178f41cd2d89d42153af001916
Freddie Wright <fpjwright@gmail.com>
2015-08-09 22:44:01 +0100

    Try simulating the game several times. This does not give an answer accurate enough.

--- problem205.java
+++ problem205.java
@@ -1,9 +1,31 @@
 import java.lang.Math;
+import java.util.Random;
 
 public class problem205 {
 
     public static void main(String[] args) {
-        System.out.println();
+        System.out.printf("%.7f", solve(Long.parseLong(args[0])));
     }
-    
+
+    public static double solve(long numberOfTries){
+        Random rn = new Random();
+        int colin;
+        int peter;
+        long peterWins = 0;
+        for(long i = 0; i<numberOfTries; i++){
+            peter = 0;
+            for(int j = 0; j<9; j++){
+                peter += rn.nextInt(4) + 1;
+            }
+            colin = 0;
+            for(int k = 0; k<6; k++){
+                colin += rn.nextInt(6) + 1;
+            }
+            if(peter > colin){
+                peterWins++;
+            }
+        }
+        return (double)peterWins/(double)numberOfTries;
+    }
+
 }
