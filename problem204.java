3da23696037000dff6632e074c0cb4a5c5ae43a7
Freddie Wright <fpjwright@gmail.com>
2015-08-12 22:55:04 +0100

    Completed problem. TODO: Find a way which doesn't produce duplicates.

--- problem204.java
+++ problem204.java
@@ -1,9 +1,24 @@
 import java.lang.Math;
+import java.util.HashSet;
 
 public class problem204 {
 
     public static void main(String[] args) {
-        System.out.println(solve());
+        count(1, 0);
+        System.out.println(totals.size());
     }
 
+    public static HashSet<Long> totals = new HashSet<Long>();
+
+    public static long[] primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 87, 89, 97, 103};
+
+    public static void count(long currentTotal, int whichPrime){
+        if(primes[whichPrime] < 100){
+            while(currentTotal <= Math.pow(10,9)){
+                count(currentTotal, whichPrime + 1);
+                totals.add(currentTotal);
+                currentTotal *= primes[whichPrime];
+            }
+        }
+    }
 }
