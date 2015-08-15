12ccf7e7a201fab9108dfd5a395628023b407d71
Freddie Wright <fpjwright@gmail.com>
2015-08-16 00:13:50 +0100

    Initial commit

---
+++ problem206.java
@@ -0,0 +1,13 @@
+import java.lang.Math;
+
+public class problem206 {
+
+    public static void main(String[] args) {
+        System.out.println(solve());
+    }
+
+    public static void solve(){
+        
+    }
+
+}
